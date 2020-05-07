c6c66d1049e2f87fb70e03812eda2b5e89b20170
shotaioramashvili <sh.ioramashvili@adjarabet.com>
2020-05-07 14:36:04 +0400

    add cancelable protocol

--- Mobile/Data/NetworkClinet/AdjarabetCoreClient/Services/AdjarabetCoreClient+BalanceManagementServices.swift
+++ Mobile/Data/NetworkClinet/AdjarabetCoreClient/Services/AdjarabetCoreClient+BalanceManagementServices.swift
@@ -7,13 +7,13 @@
 //
 
 extension AdjarabetCoreClient: BalanceManagementServices {
-    public func balance<T: AdjarabetCoreCodableType>(userId: Int, currencyId: Int, isSingle: Int, sessionId: String, completion: @escaping (Result<T, Error>) -> Void) {
+    public func balance<T: AdjarabetCoreCodableType>(userId: Int, currencyId: Int, isSingle: Int, sessionId: String, completion: @escaping (Result<T, Error>) -> Void) -> Cancellable {
         let request = standartRequestBuilder
             .set(method: .balance)
             .set(userId: userId, currencyId: currencyId, isSingle: isSingle)
             .setHeader(key: .cookie, value: sessionId)
             .build()
 
-        performTask(request: request, type: T.self, completion: completion)
+        return performTask(request: request, type: T.self, completion: completion)
     }
 }
--- Mobile/Data/NetworkClinet/AdjarabetCoreClient/Services/AdjarabetCoreClient+SessionManagementServices.swift
+++ Mobile/Data/NetworkClinet/AdjarabetCoreClient/Services/AdjarabetCoreClient+SessionManagementServices.swift
@@ -7,13 +7,13 @@
 //
 
 extension AdjarabetCoreClient: SessionManagementServices {
-    public func aliveSession<T: AdjarabetCoreCodableType>(userId: Int, sessionId: String, completion: @escaping (Result<T, Error>) -> Void) {
+    public func aliveSession<T: AdjarabetCoreCodableType>(userId: Int, sessionId: String, completion: @escaping (Result<T, Error>) -> Void) -> Cancellable {
         let request = standartRequestBuilder
             .set(method: .aliveSession)
             .set(userId: userId)
             .setHeader(key: .cookie, value: sessionId)
             .build()
 
-        performTask(request: request, type: T.self, completion: completion)
+        return performTask(request: request, type: T.self, completion: completion)
     }
 }
