fe5d4a3eb9c794a1059306b17ecde86274956887
shotaioramashvili <sh.ioramashvili@adjarabet.com>
2020-05-08 15:49:18 +0400

    cleanup promotions page

--- Mobile/Presentation/Promotions/PromotionsViewController.swift
+++ Mobile/Presentation/Promotions/PromotionsViewController.swift
@@ -12,9 +12,6 @@ public class PromotionsViewController: UIViewController {
     @Inject(from: .viewModels) private var viewModel: PromotionsViewModel
     private let disposeBag = DisposeBag()
 
-    @IBOutlet public weak var stackView: UIStackView!
-    @IBOutlet public weak var scrollView: UIScrollView!
-
     public override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
 
     public override func viewDidLoad() {
@@ -23,82 +20,6 @@ public class PromotionsViewController: UIViewController {
         setup()
         bind(to: viewModel)
         viewModel.viewDidLoad()
-
-        scrollView.contentInset.bottom = 100
-
-        func make(sw: UIStackView, text: String, style: DesignSystem.Button.Style, size: DesignSystem.Button.Size) {
-            let button = ABButton(style: style)
-            button.setSize(to: size)
-            button.setTitleWithoutAnimation(text, for: .normal)
-            sw.addArrangedSubview(button)
-        }
-
-        func make(c: (UIStackView) -> Void) {
-            let s = UIStackView()
-            s.alignment = .center
-            s.distribution = .fill
-            s.axis = .vertical
-            s.spacing = 10
-
-            stackView.addArrangedSubview(s)
-
-            c(s)
-        }
-
-        make { sw in
-            DesignSystem.Button.Size.allCases.forEach {
-                make(sw: sw, text: "DEFAULT", style: .primary(state: .normal), size: $0)
-                make(sw: sw, text: "HOVERED", style: .primary(state: .hovered), size: $0)
-                make(sw: sw, text: "ACTIVE", style: .primary(state: .acvite), size: $0)
-                make(sw: sw, text: "FOCUSED", style: .primary(state: .focused), size: $0)
-                make(sw: sw, text: "DISABLED", style: .primary(state: .disabled), size: $0)
-                make(sw: sw, text: "LOADING", style: .primary(state: .loading), size: $0)
-            }
-        }
-
-        make { sw in
-            DesignSystem.Button.Size.allCases.forEach {
-                make(sw: sw, text: "DEFAULT", style: .secondary(state: .normal), size: $0)
-                make(sw: sw, text: "HOVERED", style: .secondary(state: .hovered), size: $0)
-                make(sw: sw, text: "ACTIVE", style: .secondary(state: .acvite), size: $0)
-                make(sw: sw, text: "FOCUSED", style: .secondary(state: .focused), size: $0)
-                make(sw: sw, text: "DISABLED", style: .secondary(state: .disabled), size: $0)
-                make(sw: sw, text: "LOADING", style: .secondary(state: .loading), size: $0)
-            }
-        }
-
-        make { sw in
-            DesignSystem.Button.Size.allCases.forEach {
-                make(sw: sw, text: "DEFAULT", style: .tertiary(state: .normal), size: $0)
-                make(sw: sw, text: "HOVERED", style: .tertiary(state: .hovered), size: $0)
-                make(sw: sw, text: "ACTIVE", style: .tertiary(state: .acvite), size: $0)
-                make(sw: sw, text: "FOCUSED", style: .tertiary(state: .focused), size: $0)
-                make(sw: sw, text: "DISABLED", style: .tertiary(state: .disabled), size: $0)
-                make(sw: sw, text: "LOADING", style: .tertiary(state: .loading), size: $0)
-            }
-        }
-
-        make { sw in
-            DesignSystem.Button.Size.allCases.forEach {
-                make(sw: sw, text: "DEFAULT", style: .outline(state: .normal), size: $0)
-                make(sw: sw, text: "HOVERED", style: .outline(state: .hovered), size: $0)
-                make(sw: sw, text: "ACTIVE", style: .outline(state: .acvite), size: $0)
-                make(sw: sw, text: "FOCUSED", style: .outline(state: .focused), size: $0)
-                make(sw: sw, text: "DISABLED", style: .outline(state: .disabled), size: $0)
-                make(sw: sw, text: "LOADING", style: .outline(state: .loading), size: $0)
-            }
-        }
-
-        make { sw in
-            DesignSystem.Button.Size.allCases.forEach {
-                make(sw: sw, text: "DEFAULT", style: .ghost(state: .normal), size: $0)
-                make(sw: sw, text: "HOVERED", style: .ghost(state: .hovered), size: $0)
-                make(sw: sw, text: "ACTIVE", style: .ghost(state: .acvite), size: $0)
-                make(sw: sw, text: "FOCUSED", style: .ghost(state: .focused), size: $0)
-                make(sw: sw, text: "DISABLED", style: .ghost(state: .disabled), size: $0)
-                make(sw: sw, text: "LOADING", style: .ghost(state: .loading), size: $0)
-            }
-        }
     }
 
     private func setup() {
