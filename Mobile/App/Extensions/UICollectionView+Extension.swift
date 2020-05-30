b3b7c9f279b3e14bd421b5957cbff15365f6cd74
shotaioramashvili <sh.ioramashvili@adjarabet.com>
2020-05-30 12:57:18 +0400

    code cleanup

---
+++ Mobile/App/Extensions/UICollectionView+Extension.swift
@@ -0,0 +1,11 @@
+//
+//  UICollectionView+Extension.swift
+//  Mobile
+//
+//  Created by Shota Ioramashvili on 5/30/20.
+//  Copyright © 2020 Adjarabet. All rights reserved.
+//
+
+public extension UICollectionView {
+    var flowLayout: UICollectionViewFlowLayout? { collectionViewLayout as? UICollectionViewFlowLayout }
+}
