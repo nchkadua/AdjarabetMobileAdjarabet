3f4fb7d44b803f27cd2ed897ddd0fe0989ca04e3
shotaioramashvili <62948278+shotaioramashvili@users.noreply.github.com>
2020-06-23 17:42:46 +0400

    change fastlane script (#30)
    * change fastlane script
    
    * run swiftlint conditionality

--- Mobile/App/BuildPhases/Swiftlint.sh
+++ Mobile/App/BuildPhases/Swiftlint.sh
@@ -1,1 +1,9 @@
+#"${PODS_ROOT}/SwiftLint/swiftlint"
+
+if [ "$CONFIGURATION" == "Release-Stage" ] || [ "$CONFIGURATION" == "Release-Production" ]
+then
+echo "swiftlint was not executed"
+else
 "${PODS_ROOT}/SwiftLint/swiftlint"
+echo "swiftlint in action"
+fi
