54e7e2c721039025070ccc9dd4069796db612a64
Giorgi Kratsashvili <giorgi@Giorgis-MacBook-Pro.local>
2020-12-21 18:39:22 +0400

    Fix 2. add set(key:, value:) to Core Builder, also refactor Core Builder and make some functionallity depricated

--- Mobile/Data/NetworkClinet/AdjarabetCoreClient/AdjarabetCoreClientRequestBuilder.swift
+++ Mobile/Data/NetworkClinet/AdjarabetCoreClient/AdjarabetCoreClientRequestBuilder.swift
@@ -6,63 +6,36 @@
 //  Copyright © 2020 Adjarabet. All rights reserved.
 //
 
-public class AdjarabetCoreClientRequestBuilder: Builder {
-    public typealias Buildable = URLRequest
-
+public class AdjarabetCoreClientRequestBuilder {
     @Inject private var requestBuilder: HttpRequestBuilder
     @Inject private var userAgentProvider: UserAgentProvider
 
-    private var url: URL
-    private var queryItems: [String: String] = [:]
+    private var url = AdjarabetEndpoints.coreAPIUrl
+
     private var headers = [
         "Origin": AppConstant.coreOriginDomain,
         "Referer": AppConstant.coreOriginDomain,
         "X-Requested-With": "XMLHttpRequest"
     ]
-
-    public init(url: URL = AdjarabetEndpoints.coreAPIUrl) {
-        self.url = url
-    }
-
-    public func set(method: AdjarabetCoreClient.Method) -> Self {
-        queryItems[Key.req.rawValue] = method.rawValue
-        return self
-    }
-
-    public func set(username: String, password: String, channel: Int) -> Self {
-        queryItems[Key.userIdentifier.rawValue] = username
-        queryItems[Key.password.rawValue] = password
-        queryItems[Key.otpDeliveryChannel.rawValue] = "\(channel)"
-        return self
-    }
-
-    public func set(userId: Int, currencyId: Int, isSingle: Int) -> Self {
-        queryItems[Key.userId.rawValue] = "\(userId)"
-        queryItems[Key.currencyId.rawValue] = "\(currencyId)"
-        queryItems[Key.isSingle.rawValue] = "\(isSingle)"
-        return self
-    }
-
-    public func set(username: String, channel: Int) -> Self {
-        queryItems[Key.userIdentifier.rawValue] = username
-        queryItems[Key.channelType.rawValue] = "\(channel)"
-        return self
-    }
-
-    public func set(userId: Int) -> Self {
-        queryItems[Key.userId.rawValue] = "\(userId)"
-        return self
-    }
-
-    public func set(username: String, code: String, loginType: LoginType) -> Self {
-        queryItems[Key.userIdentifier.rawValue] = username
-        queryItems[Key.otp.rawValue] = code
-        queryItems[Key.loginType.rawValue] = loginType.rawValue
-        return self
-    }
+    private var body = [String: String]()
 
     public func setHeader(key: HeaderKey, value: String) -> Self {
         headers[key.rawValue] = value
+        return self
+    }
+
+    public func setHeader(key: String, value: String) -> Self {
+        headers[key] = value
+        return self
+    }
+
+    public func set(key: BodyKey, value: String) -> Self {
+        body[key.rawValue] = value
+        return self
+    }
+
+    public func set(key: String, value: String) -> Self {
+        body[key] = value
         return self
     }
 
@@ -74,43 +47,11 @@ public class AdjarabetCoreClientRequestBuilder: Builder {
             .setHeader(key: "User-Agent", value: userAgentProvider.userAgent)
             .set(method: HttpMethodPost())
             .set(contentType: ContentTypeUrlEncoded())
-            .set(body: queryItems)
+            .set(body: body)
             .build()
     }
 
-    // MARK: Transaction History
-
-    public func set(fromDate: String, toDate: String) -> Self {
-        queryItems[Key.fromDate.rawValue] = fromDate
-        queryItems[Key.toDate.rawValue] = toDate
-        return self
-    }
-
-    public func set(transactionType: Int?) -> Self {
-        if let transactionType = transactionType {
-            queryItems[Key.transactionType.rawValue] = "\(transactionType)"
-        }
-        return self
-    }
-
-    public func set(maxResult: Int) -> Self {
-        queryItems[Key.maxResult.rawValue] = "\(maxResult)"
-        return self
-    }
-
-    public func set(pageIndex: Int) -> Self {
-        queryItems[Key.pageIndex.rawValue] = "\(pageIndex)"
-        return self
-    }
-
-    public func set(providerType: Int) -> Self {
-        queryItems[Key.providerType.rawValue] = "\(providerType)"
-        return self
-    }
-
-    // MARK: Keys
-
-    public enum Key: String {
+    public enum BodyKey: String {
         case userIdentifier
         case password
         case req
@@ -131,5 +72,83 @@ public class AdjarabetCoreClientRequestBuilder: Builder {
 
     public enum HeaderKey: String {
         case cookie = "Cookie"
+    }
+
+    /* deprecated part */
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(method: AdjarabetCoreClient.Method) -> Self {
+        body[BodyKey.req.rawValue] = method.rawValue
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(username: String, password: String, channel: Int) -> Self {
+        body[BodyKey.userIdentifier.rawValue] = username
+        body[BodyKey.password.rawValue] = password
+        body[BodyKey.otpDeliveryChannel.rawValue] = "\(channel)"
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(userId: Int, currencyId: Int, isSingle: Int) -> Self {
+        body[BodyKey.userId.rawValue] = "\(userId)"
+        body[BodyKey.currencyId.rawValue] = "\(currencyId)"
+        body[BodyKey.isSingle.rawValue] = "\(isSingle)"
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(username: String, channel: Int) -> Self {
+        body[BodyKey.userIdentifier.rawValue] = username
+        body[BodyKey.channelType.rawValue] = "\(channel)"
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(userId: Int) -> Self {
+        body[BodyKey.userId.rawValue] = "\(userId)"
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(username: String, code: String, loginType: LoginType) -> Self {
+        body[BodyKey.userIdentifier.rawValue] = username
+        body[BodyKey.otp.rawValue] = code
+        body[BodyKey.loginType.rawValue] = loginType.rawValue
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(fromDate: String, toDate: String) -> Self {
+        body[BodyKey.fromDate.rawValue] = fromDate
+        body[BodyKey.toDate.rawValue] = toDate
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(transactionType: Int?) -> Self {
+        if let transactionType = transactionType {
+            body[BodyKey.transactionType.rawValue] = "\(transactionType)"
+        }
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(maxResult: Int) -> Self {
+        body[BodyKey.maxResult.rawValue] = "\(maxResult)"
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(pageIndex: Int) -> Self {
+        body[BodyKey.pageIndex.rawValue] = "\(pageIndex)"
+        return self
+    }
+
+    @available(*, deprecated, message: "Please use set(key:, value:)")
+    public func set(providerType: Int) -> Self {
+        body[BodyKey.providerType.rawValue] = "\(providerType)"
+        return self
     }
 }
