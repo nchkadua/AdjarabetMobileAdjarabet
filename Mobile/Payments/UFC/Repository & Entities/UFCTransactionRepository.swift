261675b7713c4d452122daf3b77556525e909349
Giorgi Kratsashvili <g.kratsashvili@adjarabet.com>
2021-03-16 19:33:48 +0400

    add implementation of payment repository

--- Mobile/Payments/UFC/Repository & Entities/UFCTransactionRepository.swift
+++ Mobile/Payments/UFC/Repository & Entities/UFCTransactionRepository.swift
@@ -16,7 +16,7 @@ struct UFCTransactionParams {
     let serviceId: Int
     let amount: Double
     let serviceName: String
-    let saveForRecurring: Bool
+    let saveForRecurring: Int
     let accountId: Int64? // if null add card service is called
     let session: String?  // if null is init method
 }
@@ -35,7 +35,7 @@ protocol UFCDepositRepository {
     // output
     typealias InitDepositHandler = (Result<UFCInitDepositEntity, Error>) -> Void
     // input
-    func initDeposit(with params: UFCInitDepositParams, _ handler: InitDepositHandler)
+    func initDeposit(with params: UFCInitDepositParams, _ handler: @escaping InitDepositHandler)
 
     /**
      Performs Deposit Transaction
@@ -43,7 +43,7 @@ protocol UFCDepositRepository {
     // output
     typealias DepositHandler = (Result<UFCDepositEntity, Error>) -> Void
     // input
-    func deposit(with params: UFCDepositParams, _ handler: DepositHandler)
+    func deposit(with params: UFCDepositParams, _ handler: @escaping DepositHandler)
 }
 
 // Init Withdraw Parameters
@@ -60,7 +60,7 @@ protocol UFCWithdrawRepository {
     // output
     typealias InitWithdrawHandler = (Result<UFCInitWithdrawEntity, Error>) -> Void
     // input
-    func initWithdraw(with params: UFCInitWithdrawParams, _ handler: InitWithdrawHandler)
+    func initWithdraw(with params: UFCInitWithdrawParams, _ handler: @escaping InitWithdrawHandler)
 
     /**
      Performs Withdraw Transaction
@@ -68,9 +68,96 @@ protocol UFCWithdrawRepository {
     // output
     typealias WithdrawHandler = (Result<UFCWithdrawEntity, Error>) -> Void
     // input
-    func withdraw(with params: UFCWithdrawParams, _ handler: WithdrawHandler)
+    func withdraw(with params: UFCWithdrawParams, _ handler: @escaping WithdrawHandler)
 }
 
-// MARK: - Implementation
+// MARK: Deposit & Withdraw Repository
+protocol UFCTransactionRepository: UFCDepositRepository, UFCWithdrawRepository { }
 
-// ...
+// MARK: - Implementation
+struct DefaultUFCTransactionRepository: UFCTransactionRepository {
+    @Inject private var userSession: UserSessionReadableServices
+    @Inject private var languageStorage: LanguageStorage
+    private var httpRequestBuilder: HttpRequestBuilder { HttpRequestBuilderImpl.createInstance() }
+    private var dataTransferService: DataTransferService { DefaultDataTransferService() }
+
+    func initDeposit(with params: UFCInitDepositParams, _ handler: @escaping InitDepositHandler) {
+        transaction(operation: "initDeposit", with: params, expecting: UFCInitDepositDTO.self, handler)
+    }
+
+    func deposit(with params: UFCDepositParams, _ handler: @escaping DepositHandler) {
+        transaction(operation: "Deposit", with: params, expecting: UFCDepositDTO.self, handler)
+    }
+
+    func initWithdraw(with params: UFCInitWithdrawParams, _ handler: @escaping InitWithdrawHandler) {
+        transaction(operation: "initWithdraw", with: params, expecting: UFCInitWithdrawDTO.self, handler)
+    }
+
+    func withdraw(with params: UFCWithdrawParams, _ handler: @escaping WithdrawHandler) {
+        transaction(operation: "withdraw", with: params, expecting: UFCWithdrawDTO.self, handler)
+    }
+
+    private func transaction<Response: DataTransferResponse> (
+        operation: String,
+        with params: UFCTransactionParams,
+        expecting response: Response.Type,
+        _ handler: @escaping (Result<Response.Entity, Error>) -> Void
+    ) {
+        guard let sessionId = userSession.sessionId,
+              let userId = userSession.userId,
+              let currencyId = userSession.currencyId,
+              let currency = Currency(currencyId: currencyId)
+        else {
+            handler(.failure(AdjarabetCoreClientError.sessionUninitialzed))
+            return
+        }
+
+        var body: [String: Any] = [
+            "op_type": operation,
+            "user_id": userId,
+            "provider_id": params.providerId,
+            "service_name": params.serviceName,
+            "service_id": params.serviceId,
+            "currency": currency.description.abbreviation,
+            "save_for_recurring": params.saveForRecurring,
+            "lang": languageStorage.currentLanguage.localizableIdentifier,
+            "amount": "\(params.amount)"
+        ]
+
+        if let accountId = params.accountId { // if null add card service is called
+            body["account_id"] = "\(accountId)"
+        }
+
+        if let session = params.session { // if null is init method
+            body["session"] = session
+        }
+
+        let headers = [
+            "Cookie": sessionId,
+            "Origin": AppConstant.coreOriginDomain,
+            "Referer": AppConstant.coreOriginDomain,
+            "X-Requested-With": "XMLHttpRequest"
+        ]
+
+        guard let dataJson = try? JSONSerialization.data(withJSONObject: body, options: []),
+              let dataString = String(data: dataJson, encoding: .ascii)
+        else {
+            handler(.failure(AdjarabetCoreClientError.coreError(description: "Request Parsing Error")))
+            return
+        }
+
+        let request = httpRequestBuilder
+            .set(host: "https://coreapi.adjarabet.com/AuthenticationProxy")
+            .set(headers: headers)
+            .set(method: HttpMethodPost())
+            .set(contentType: ContentTypeUrlEncoded())
+            .setBody(key: "data", value: dataString)
+            .setBody(key: "req", value: operation)
+            .build()
+
+        dataTransferService.performTask(expecting: response,
+                                        request: request,
+                                        respondOnQueue: .main,
+                                        completion: handler)
+    }
+}
