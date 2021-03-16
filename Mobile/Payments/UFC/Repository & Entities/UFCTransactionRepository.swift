cccc5f708620d7341d1a895f92a72f6c55fccf3f
Giorgi Kratsashvili <g.kratsashvili@adjarabet.com>
2021-03-16 19:10:29 +0400

    create interface for payment repository

--- Mobile/Payments/UFC/Repository & Entities/UFCTransactionRepository.swift
+++ Mobile/Payments/UFC/Repository & Entities/UFCTransactionRepository.swift
@@ -9,12 +9,24 @@
 import Foundation
 
 /**
- General Paramters for UFC Transaction
+ General Parameters for UFC Transactions
  */
-protocol UFCTransactionParams {
-
+struct UFCTransactionParams {
+    let providerId: String
+    let serviceId: Int
+    let amount: Double
+    let serviceName: String
+    let saveForRecurring: Bool
+    let accountId: Int64? // if null add card service is called
+    let session: String?  // if null is init method
 }
 
+// Init Deposit Parameters
+typealias UFCInitDepositParams = UFCTransactionParams
+// Deposit Parameters
+typealias UFCDepositParams = UFCTransactionParams
+
+// MARK: Deposit Repository
 protocol UFCDepositRepository {
     /**
      Initializes Deposit Transaction:
@@ -23,5 +35,42 @@ protocol UFCDepositRepository {
     // output
     typealias InitDepositHandler = (Result<UFCInitDepositEntity, Error>) -> Void
     // input
-    func initDeposit(_ handler: InitDepositHandler)
+    func initDeposit(with params: UFCInitDepositParams, _ handler: InitDepositHandler)
+
+    /**
+     Performs Deposit Transaction
+     */
+    // output
+    typealias DepositHandler = (Result<UFCDepositEntity, Error>) -> Void
+    // input
+    func deposit(with params: UFCDepositParams, _ handler: DepositHandler)
 }
+
+// Init Withdraw Parameters
+typealias UFCInitWithdrawParams = UFCTransactionParams
+// Withdraw Parameters
+typealias UFCWithdrawParams = UFCTransactionParams
+
+// MARK: Withdraw Repository
+protocol UFCWithdrawRepository {
+    /**
+     Initializes Withdraw Transaction:
+     Returns Session ID for Transaction
+     */
+    // output
+    typealias InitWithdrawHandler = (Result<UFCInitWithdrawEntity, Error>) -> Void
+    // input
+    func initWithdraw(with params: UFCInitWithdrawParams, _ handler: InitWithdrawHandler)
+
+    /**
+     Performs Withdraw Transaction
+     */
+    // output
+    typealias WithdrawHandler = (Result<UFCWithdrawEntity, Error>) -> Void
+    // input
+    func withdraw(with params: UFCWithdrawParams, _ handler: WithdrawHandler)
+}
+
+// MARK: - Implementation
+
+// ...
