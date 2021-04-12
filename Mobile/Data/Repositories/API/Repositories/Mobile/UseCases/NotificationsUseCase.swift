383ddaa7c2427c8e57f71f2224cfa0005d8e7e78
Nika <n.tchkadua@adjarabet.com>
2021-04-12 16:18:55 +0400

    Read/Delete use cases added

--- Mobile/Data/Repositories/API/Repositories/Mobile/UseCases/NotificationsUseCase.swift
+++ Mobile/Data/Repositories/API/Repositories/Mobile/UseCases/NotificationsUseCase.swift
@@ -11,6 +11,10 @@ import Foundation
 protocol NotificationsUseCase {
     typealias NotificationsHandler = (Result<NotificationItemsEntity, Error>) -> Void
     func notifications(page: Int, domain: String, handler: @escaping NotificationsHandler)
+
+    typealias NotificationStatusUpdateHandler = (Result<NotificationStatusUpdateMessageEntity, Error>) -> Void
+    func read(notificationId: Int, handler: @escaping NotificationStatusUpdateHandler)
+    func delete(notificationId: Int, handler: @escaping NotificationStatusUpdateHandler)
 }
 
 struct DefaultNotificationsUseCase: NotificationsUseCase {
@@ -18,6 +22,24 @@ struct DefaultNotificationsUseCase: NotificationsUseCase {
 
     func notifications(page: Int, domain: String, handler: @escaping NotificationsHandler) {
         repo.notifications(params: .init(page: page, domain: domain)) { result in
+            switch result {
+            case .success(let entity): handler(.success(entity))
+            case .failure(let error): handler(.failure(error))
+            }
+        }
+    }
+
+    func read(notificationId: Int, handler: @escaping NotificationStatusUpdateHandler) {
+        repo.read(params: .init(notifiationId: notificationId)) { result in
+            switch result {
+            case .success(let entity): handler(.success(entity))
+            case .failure(let error): handler(.failure(error))
+            }
+        }
+    }
+
+    func delete(notificationId: Int, handler: @escaping NotificationStatusUpdateHandler) {
+        repo.read(params: .init(notifiationId: notificationId)) { result in
             switch result {
             case .success(let entity): handler(.success(entity))
             case .failure(let error): handler(.failure(error))
