13258d4b0ee8d4a3af8e670a81c064dda9722736
Giorgi Kratsashvili <g.kratsashvili@adjarabet.com>
2021-04-13 15:38:52 +0400

    link outlets

--- Mobile/Presentation/MainPages/Profile/CashFlow/Withdraw/Scenes/Visa/Views/Cash Out/CashOutVisaView.swift
+++ Mobile/Presentation/MainPages/Profile/CashFlow/Withdraw/Scenes/Visa/Views/Cash Out/CashOutVisaView.swift
@@ -9,8 +9,22 @@
 import UIKit
 
 class CashOutVisaView: UIView, Xibable {
-    /* Main View */
     @IBOutlet private weak var view: UIView!
+    @IBOutlet private weak var amountInputView: ABInputView!
+    @IBOutlet private weak var accountPickerView: ABInputView!
+    @IBOutlet private weak var addAccountButton: UIButton!
+    /* Summary */
+    @IBOutlet private weak var summaryView: UIView!
+    // Fee
+    @IBOutlet private weak var feeTitleLabel: UILabel!
+    @IBOutlet private weak var feeAmountView: UIView!
+    @IBOutlet private weak var feeAmountLabel: UILabel!
+    // Total
+    @IBOutlet private weak var totalTitleLabel: UILabel!
+    @IBOutlet private weak var totalAmountView: UIView!
+    @IBOutlet private weak var totalAmountLabel: UILabel!
+    /* Continue */
+    @IBOutlet private weak var continueButton: ABButton!
 
     var mainView: UIView {
         get { view }
