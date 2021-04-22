e731119424160dea92c83a9ee51982011f9fce17
Giorgi Kratsashvili <g.kratsashvili@adjarabet.com>
2021-04-22 19:43:06 +0400

    add feature to HttpRequestBuilder

--- MobileTests/Infrastructure/HttpRequestBuilder/HttpRequestBuilderTests.swift
+++ MobileTests/Infrastructure/HttpRequestBuilder/HttpRequestBuilderTests.swift
@@ -32,6 +32,7 @@ class HttpRequestBuilderTests: XCTestCase {
     }
 
     /* Test set(path:) */
+
     func testPath() {
         // when
         let request = requestBuilder
@@ -42,6 +43,86 @@ class HttpRequestBuilderTests: XCTestCase {
 
         // than
         XCTAssertEqual(request.url?.absoluteString, "\(host)/\(path)")
+    }
+
+    /* Test setUrlParam(key:, value:) */
+
+    func testSet1UrlParam() {
+        // when
+        let key   = "Key"
+        let value = "Value"
+
+        let request = requestBuilder
+            .set(host: host)
+            .set(path: path)
+            .setUrlParam(key: key, value: value)
+            .set(method: HttpMethodGet())
+            .build()
+
+        // than
+        XCTAssertEqual(request.url?.absoluteString, "\(host)/\(path)%3F\(key)=\(value)")
+    }
+
+    func testSet2UrlParam() {
+        // when
+        let key1 = "Key1"
+        let val1 = "Value1"
+
+        let key2 = "Key2"
+        let val2 = "Value2"
+
+        let request = requestBuilder
+            .set(host: host)
+            .set(path: path)
+            .setUrlParam(key: key1, value: val1)
+            .setUrlParam(key: key2, value: val2)
+            .set(method: HttpMethodGet())
+            .build()
+
+        // than
+        let actual = request.url?.absoluteString
+
+        let expected1 = "\(host)/\(path)%3F\(key1)=\(val1)&\(key2)=\(val2)"
+        let expected2 = "\(host)/\(path)%3F\(key2)=\(val2)&\(key1)=\(val1)"
+
+        XCTAssert(actual == expected1 || actual == expected2)
+    }
+
+    /* Test set(urlParams:) */
+    
+    func testSetUrlParams() {
+        // when
+        let urlParams = [
+            "Key1": "Value1",
+            "Key2": "Value2",
+            "Key3": "Value3",
+        ]
+
+        let request = requestBuilder
+            .set(host: host)
+            .set(path: path)
+            .set(urlParams: urlParams)
+            .set(method: HttpMethodGet())
+            .build()
+
+        // than
+        let actual = request.url?.absoluteString
+
+        let urlParamsList = urlParams.map { ($0.key, $0.value) }
+
+        for permutaion in permutations(of: urlParamsList) {
+            var possibleExpected = "\(host)/\(path)%3F"
+            for (key, value) in permutaion {
+                possibleExpected.append("\(key)=\(value)&")
+            }
+            possibleExpected.removeLast() // remove last &
+            if possibleExpected == actual {
+                XCTAssert(true)
+                return
+            }
+        }
+
+        XCTAssert(false)
     }
 
     /* Test setHeader(key:, value:) */
@@ -191,5 +272,25 @@ class HttpRequestBuilderTests: XCTestCase {
         let expected = stringData
         let actual   = String(decoding: request.httpBody ?? .init(), as: UTF8.self)
         XCTAssertEqual(expected, actual)
+    }
+
+    // helpers
+    private func permutations<T: Any>(of list: [T]) -> [[T]] {
+        if list.count < 2 {
+            return [list]
+        }
+        var result: [[T]] = []
+        list.enumerated().forEach { i, e in
+            var nextList = list
+            nextList.swapAt(0, i)
+            let firstElem = nextList.remove(at: 0)
+            let subResult = permutations(of: nextList)
+            result.append(contentsOf: subResult.map {
+                var list = $0
+                list.insert(firstElem, at: 0)
+                return list
+            })
+        }
+        return result
     }
 }
