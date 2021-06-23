2378933c0d817ca00ee618db2109d4707e00e7e5
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2021-06-23 18:34:48 +0400

    rearrange files

---
+++ Mobile/Scenes/Profile/Account Information/Scenes/Mail Change/Data/Use Cases/UpdateMailUseCase.swift
@@ -0,0 +1,9 @@
+//
+//  UpdateMailUseCase.swift
+//  Mobile
+//
+//  Created by Giorgi Kratsashvili on 6/23/21.
+//  Copyright © 2021 Adjarabet. All rights reserved.
+//
+
+import Foundation
---
+++ Mobile/Scenes/Profile/Account Information/Scenes/Phone Number Change/Data/Use Cases/UpdatePhoneNumberUseCase.swift
@@ -0,0 +1,9 @@
+//
+//  UpdatePhoneNumberUseCase.swift
+//  Mobile
+//
+//  Created by Giorgi Kratsashvili on 6/23/21.
+//  Copyright © 2021 Adjarabet. All rights reserved.
+//
+
+import Foundation
