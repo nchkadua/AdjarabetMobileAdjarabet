214476356e104494710b9ee67021cbac15d7d2e3
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2021-06-25 13:03:32 +0400

    string localization left

--- Mobile/Scenes/Profile/Account/Parameters/High Security/Presentation/HighSecurityViewController.swift
+++ Mobile/Scenes/Profile/Account/Parameters/High Security/Presentation/HighSecurityViewController.swift
@@ -12,11 +12,50 @@ class HighSecurityViewController: ABPopupViewController {
     @Inject(from: .viewModels) private var viewModel: HighSecurityViewModel
     private lazy var navigator = HighSecurityNavigator(viewController: self)
 
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+
+    private var isOn = false // FIXME: temporary button state
+    @IBOutlet private weak var onOffButton: UIButton!
+
     // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         bind(to: viewModel)
+        setup()
         viewModel.viewDidLoad()
+    }
+
+    private func setup() {
+        view.setBackgorundColor(to: .secondaryBg())
+
+        titleLabel.setTextColor(to: .primaryText())
+        titleLabel.setFont(to: .body1(fontCase: .lower, fontStyle: .semiBold))
+        titleLabel.text = "მაღალი დაცვის რეჟიმი"
+
+        descriptionLabel.setTextColor(to: .secondaryText())
+        descriptionLabel.setFont(to: .footnote(fontCase: .lower, fontStyle: .regular))
+        descriptionLabel.text = "მაღალი დაცვის რეჟიმის გააქტიურებით, თქვენი აჭარაბეთის ანგარიში ბევრად უფრო დაცულია. ავტორიზაცია და პერსონალურ ინფორმაციასთან წვდომა ხდება მხოლოდ უსაფრთხოების კოდის გამოყენებით."
+
+        onOffButton.setBackgorundColor(to: .tertiaryBg())
+        onOffButton.titleLabel?.font = DesignSystem.Typography.callout(fontCase: .upper, fontStyle: .semiBold).description.font
+        onOffButton.roundCorners(.allCorners, radius: 8)
+        setupOnOffButtonState()
+    }
+
+    private func setupOnOffButtonState() {
+        if isOn {
+            onOffButton.setTitleColor(.systemPink, for: .normal)
+            onOffButton.setTitle("გათიშვა".uppercased(), for: .normal)
+        } else {
+            onOffButton.setTitleColor(.systemBlue, for: .normal)
+            onOffButton.setTitle("გააქტიურება".uppercased(), for: .normal)
+        }
+    }
+
+    @IBAction private func onOffButtonTapped() {
+        isOn.toggle()
+        setupOnOffButtonState()
     }
 
     // MARK: Bind to viewModel's observable properties
@@ -44,7 +83,7 @@ extension HighSecurityViewController: UIViewControllerTransitioningDelegate {
             presentedViewController: presented,
             presenting: presenting,
             params: .init(
-                heightConstant: 240
+                heightConstant: 280
             )
         )
     }
